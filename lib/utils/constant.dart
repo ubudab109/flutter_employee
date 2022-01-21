4874e8a376ef70b5e5f599bcb2d34a4e3072c1e2
Muhammad Rizky Firdaus <62287144+ubudab109@users.noreply.github.com>
2022-01-21 21:57:01 +0700

    gitignore

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color.fromRGBO(255, 255, 255, 1);
 const kBluePrimary = Color.fromRGBO(25, 200, 255, 1);
 const themePrimayColor = Color(0XFFE5E5E5);
-const urlBackend = 'http://10.0.2.2:8000/api'; //change this with your own
+const urlBackend = 'http://192.168.1.13:8000/api'; //change this with your own
